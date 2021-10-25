309305b849b994282e626dace7fded65adebc93d
keyjjang5 <keyjjang5@naver.com>
2021-10-25 14:01:03 +0900

    각종 행동 추가, 부드러운 행동 연결, 커맨드 구조 변경
    딕셔너리를 이용해서 이동, 커맨드 입력을 처리하는 방식으로 변경함, 왼손, 왼발, 오른손, 오른발에 대응하는 키를 입력 받아서 각각의 커맨드에 따른 행동을 하도록 제작, Mixamo에서 받은 애니메이션을 기존 모델에 연결하여 사용, 상태에 따른 행동가능 영역 설정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     int postCommand = 0;
     float postCommandTime = 0;
 
+    State state;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,58 +29,23 @@ public class PlayerController : MonoBehaviour
         center = GameObject.Find("CenterPoint").transform;
 
         commands = new Queue<int>();
+
+        state = State.Standing;
     }
 
     
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.J))
-            //ActiveCommand();
-
-        /*
-        // 기본적인 이동 211010
-        if (Input.GetKey(KeyCode.D))
-        {
-            //Debug.Log("정면으로 걷기");
-            velocity = Vector3.forward;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            //Debug.Log("후면으로 걷기");
-            velocity = Vector3.back;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            //Debug.Log("좌측면으로 걷기");
-            velocity = Vector3.left;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            //Debug.Log("우측면으로 걷기");
-            velocity = Vector3.right;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        else
-        {
-            animator.SetBool("Walk", false);
-        }
-        */
+        // 액션 실행
+        if (Input.GetKeyDown(KeyCode.U))
+            activeLP();
+        else if (Input.GetKeyDown(KeyCode.I))
+            activeRP();
+        else if (Input.GetKeyDown(KeyCode.J))
+            activeLK();
+        else if (Input.GetKeyDown(KeyCode.K))
+            activeRK();
 
         transform.LookAt(center);
 
@@ -107,7 +73,7 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
 
         // 커맨드 입력
-        if (horizontal > 0)
+        /*if (horizontal > 0)
         {
             if (vertical > 0)
             {
@@ -178,81 +144,104 @@ public class PlayerController : MonoBehaviour
             if (postCommand != 5)
                 commandChange(5);
         }
+        */
+        // Vector2(horizontal, vertical)
+        Dictionary<Vector2, Vector3> horiVer = new Dictionary<Vector2, Vector3>()
+        {
+            {new Vector2(-1, -1), Vector3.back + Vector3.right },
+            {new Vector2(-1, 0), Vector3.back },
+            {new Vector2(-1, 1), Vector3.back + Vector3.left },
+
+            {new Vector2(0, -1), Vector3.right },
+            {new Vector2(0, 0), Vector3.zero },
+            {new Vector2(0, 1), Vector3.left },
+
+            {new Vector2(1, -1), Vector3.forward + Vector3.right },
+            {new Vector2(1, 0), Vector3.forward },
+            {new Vector2(1, 1), Vector3.forward + Vector3.left }
+        };
+
+        Dictionary<Vector2, int> horiVerCommand = new Dictionary<Vector2, int>()
+        {
+            {new Vector2(-1, -1), 1 },
+            {new Vector2(-1, 0), 4 },
+            {new Vector2(-1, 1), 7 },
+
+            {new Vector2(0, -1), 2 },
+            {new Vector2(0, 0), 5 },
+            {new Vector2(0, 1), 8 },
+
+            {new Vector2(1, -1), 3 },
+            {new Vector2(1, 0), 6 },
+            {new Vector2(1, 1), 9 }
+        };
+
+        horiVerCommand.TryGetValue(new Vector2(horizontal, vertical), out int command);
+        if (postCommand != command)
+            commandChange(command);
+
+        if (state == State.Standing)
+        {
+            // 이동 영역
+            Vector3 velo = Vector3.zero;
+            horiVer.TryGetValue(new Vector2(horizontal, vertical), out velo);
+
+            velo = Vector3.Lerp(currentVelocity, velo, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
+            transform.Translate(velo);
+
+            animator.SetBool("Walk", true);
+            if(velo == Vector3.zero)
+                animator.SetBool("Walk", false);
 
 
+            // 커맨드 영역
+            if (command == 6)
+                if (searchCommands("656") && animator.GetBool("FrontDash") == false)
+                    frontDash();
+            if (command == 4)
+                if (searchCommands("454") && animator.GetBool("BackDash") == false)
+                    backDash();
+        }
         // horizontal, vertical을 통한 이동
-        if (horizontal > 0)
-        {
-            //Debug.Log("정면으로 걷기");
-            velocity = Vector3.forward;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        if (horizontal < 0)
-        {
-            //Debug.Log("후면으로 걷기");
-            velocity = Vector3.back;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        if (vertical > 0)
-        {
-            //Debug.Log("좌측면으로 걷기");
-            velocity = Vector3.left;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        if (vertical < 0)
-        {
-            //Debug.Log("우측면으로 걷기");
-            velocity = Vector3.right;
-            velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
-            transform.Translate(velocity);
-
-            animator.SetBool("Walk", true);
-        }
-        if (vertical == 0 && horizontal == 0)
-        {
-            animator.SetBool("Walk", false);
-        }
-        // 누적 커맨드 표시
-        //string s = null;
-        //foreach (int i in commands)
-        //    s += i;
-        //Debug.Log("commands : :" + s);
-
-        //Debug.Log("verticla : " + vertical);
-        //Debug.Log("Horizontal : " +horizontal);
-
-        //int j = 0;
-        //foreach (char c in Input.inputString)
+        //if (horizontal > 0)
         //{
-        //    commands.Enqueue(c);
-        //    if (commands.Count > 10)
-        //        commands.Dequeue();
-        //    j++;
+        //    //Debug.Log("정면으로 걷기");
+        //    velocity = Vector3.forward;
+        //    velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
+        //    transform.Translate(velocity);
+
+        //    animator.SetBool("Walk", true);
         //}
-
-        //if (!(j > 0))
+        //if (horizontal < 0)
         //{
-        //    commands.Enqueue('*');
-        //    if (commands.Count > 10)
-        //        commands.Dequeue();
+        //    //Debug.Log("후면으로 걷기");
+        //    velocity = Vector3.back;
+        //    velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
+        //    transform.Translate(velocity);
+
+        //    animator.SetBool("Walk", true);
         //}
-        //else
-        //    j = 0;
-
-        //int i = 0;
-        //foreach (char c in commands)
+        //if (vertical > 0)
         //{
-        //    Debug.Log("Commands[" + i + "] : " + c);
-        //    i++;
+        //    //Debug.Log("좌측면으로 걷기");
+        //    velocity = Vector3.left;
+        //    velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
+        //    transform.Translate(velocity);
+
+        //    animator.SetBool("Walk", true);
+        //}
+        //if (vertical < 0)
+        //{
+        //    //Debug.Log("우측면으로 걷기");
+        //    velocity = Vector3.right;
+        //    velocity = Vector3.Lerp(currentVelocity, velocity, Mathf.Min(Time.deltaTime * 5.0f, 1.0f));
+        //    transform.Translate(velocity);
+
+        //    animator.SetBool("Walk", true);
+        //}
+        //if (vertical == 0 && horizontal == 0)
+        //{
+        //    animator.SetBool("Walk", false);
         //}
     }
 
@@ -290,23 +279,107 @@ public class PlayerController : MonoBehaviour
         postCommand = 0;
     }
 
+    // 왼손 공격 탐색, 실행
+    void activeLP()
+    {
+        // 선자세
+        if (state == State.Standing)
+        {
+            if (searchCommands("56"))
+                animator.SetInteger("LPunch", 2);
+            else if (searchCommands("53"))
+                animator.SetInteger("LPunch", 3);
+            else
+                animator.SetInteger("LPunch", 1);
+            state = State.Attacking;
+        }
+
+        clearCommands();
+    }
+
+    // 오른손 공격 탐색, 실행
+    void activeRP()
+    {
+        // 선자세
+        if (state == State.Standing)
+        {
+            if (searchCommands("56"))
+                animator.SetInteger("RPunch", 2);
+            else if (searchCommands("53"))
+                animator.SetInteger("RPunch", 3);
+            else
+                animator.SetInteger("RPunch", 1);
+            state = State.Attacking;
+        }
+
+        clearCommands();
+    }
+
+    // 왼발 공격 탐색, 실행
+    void activeLK()
+    {
+        // 선자세
+        if (state == State.Standing)
+        {
+            if (searchCommands("59"))
+                animator.SetInteger("LKick", 2);
+            else
+                animator.SetInteger("LKick", 1);
+            state = State.Attacking;
+            animator.SetBool("Walk", false);
+        }
+
+        clearCommands();
+    }
+
+    // 오른발 공격 탐색, 실행
+    void activeRK()
+    {
+        // 선자세
+        if (state == State.Standing)
+        {
+            if (searchCommands("52"))
+                animator.SetInteger("RKick", 2);
+            else
+                animator.SetInteger("RKick", 1);
+            state = State.Attacking;
+        }
+
+        clearCommands();
+    }
+
     // 앞대시
     void frontDash()
     {
         animator.SetBool("FrontDash", true);
-        Debug.Log("전진 대시");
     }
 
     // 백대시
     void backDash()
     {
-
+        animator.SetBool("BackDash", true);
     }
 
     void DashEnd()
     {
         animator.SetBool("FrontDash", false);
+        animator.SetBool("BackDash", false);
         clearCommands();
+    }
+
+    void AttackEnd()
+    {
+        animator.SetInteger("LPunch", 0);
+        animator.SetInteger("RPunch", 0);
+        animator.SetInteger("LKick", 0);
+        animator.SetInteger("RKick", 0);
+
+        state = State.Standing;
+    }
+
+    void Hit()
+    {
+        
     }
 
     void FootR()
@@ -317,4 +390,17 @@ public class PlayerController : MonoBehaviour
     {
 
     }
+}
+
+// 상태
+enum State
+{
+    // 선자세
+    Standing,
+    // 앉은자세
+    Sitting,
+    // 공격 중
+    Attacking,
+    // 맞는 중
+    Hited
 }
